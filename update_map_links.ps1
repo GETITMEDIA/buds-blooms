79d19ccfacdec3942057c63603b4d53d0b546ae7
Siva1410 <sjavxbs@gmail.com>
2026-08-20 12:01:41 +0530

    crtttrtrtrtrtrtrtrr

--- update_map_links.ps1
+++
@@ -1,9 +0,0 @@
-$files = Get-ChildItem -Filter *.html
-foreach ($file in $files) {
-    $content = Get-Content $file.FullName -Raw
-
-    $content = $content.Replace("https://maps.app.goo.gl/9ik651KTJ5uJDEZBA", "https://maps.app.goo.gl/hKR8bkc7g2gktQFs7")
-    $content = $content.Replace("https://maps.app.goo.gl/pDqgLvQdyULwN5pz7", "https://maps.app.goo.gl/tjVZiM3XDLjABWuc9")
-
-    [System.IO.File]::WriteAllText($file.FullName, $content, (New-Object System.Text.UTF8Encoding($false)))
-}
